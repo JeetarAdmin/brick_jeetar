5a27086955f9cedd4ca7294be725eec2248a532c
DAMMAK <adekoyafelix@gmail.com>
2022-07-05 14:51:26 +0100

    animation navigation added

---
+++ bricks/starter_setup/__brick__/libs/shared/navigation/animations/slide_down.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SlideDownRoute extends PageRouteBuilder {
+  final Widget page;
+  SlideDownRoute({required this.page})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1, 0),
+              end: const Offset(0, 0),
+            ).animate(animation),
+            child: child,
+          ),
+        );
+}
