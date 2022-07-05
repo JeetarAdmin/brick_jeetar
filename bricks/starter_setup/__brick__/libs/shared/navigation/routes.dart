5620faaf05a2f56f33deb73bed1fdad208794ff2
DAMMAK <adekoyafelix@gmail.com>
2022-07-05 19:58:18 +0100

    navigation routes

--- bricks/starter_setup/__brick__/libs/shared/navigation/routes.dart
+++ bricks/starter_setup/__brick__/libs/shared/navigation/routes.dart
@@ -1,16 +1,8 @@
-
 // ignore: prefer_function_declarations_over_variables
+import 'package:flutter/material.dart';
+
 var routes = (RouteSettings settings) {
   switch (settings.name) {
-    case RootRoutes.initial:
-      return FadeRoute(
-        page: const SplashScreen(),
-      );
-    case RootRoutes.home:
-      return SlideUpRoute(page: const RootWidget());
-
-    case HomeRoutes.viewStores:
-      return SlideUpRoute(page: const Store());
   }
 
   return null;
