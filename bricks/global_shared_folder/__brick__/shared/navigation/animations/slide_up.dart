7cc07dcc8692ee8bf877202765f71b346aa01fe9
DAMMAK <adekoyafelix@gmail.com>
2022-07-05 13:28:21 +0100

    navigation animation added

---
+++ bricks/global_shared_folder/__brick__/shared/navigation/animations/slide_up.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SlideUpRoute extends PageRouteBuilder {
+  final Widget page;
+  SlideUpRoute({required this.page})
+      : super(
+            pageBuilder: (
+              BuildContext context,
+              Animation<double> animation,
+              Animation<double> secondaryAnimation,
+            ) =>
+                page,
+            transitionsBuilder: (
+              BuildContext context,
+              Animation<double> animation,
+              Animation<double> secondaryAnimation,
+              Widget child,
+            ) {
+              const begin = Offset(0.0, 1.0);
+              const end = Offset.zero;
+              const curve = Curves.ease;
+
+              var tween =
+                  Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+              return SlideTransition(
+                position: animation.drive(tween),
+                child: child,
+              );
+            });
+}
