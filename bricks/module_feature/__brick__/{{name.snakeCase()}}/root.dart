dc8da03262db7ed2177bfe39f0203be93c1c9b3a
DAMMAK <adekoyafelix@gmail.com>
2022-08-24 18:15:51 +0100

    fixes on root widget

--- bricks/module_feature/__brick__/{{name.snakeCase()}}/root.dart
+++ bricks/module_feature/__brick__/{{name.snakeCase()}}/root.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 
 class {{name.pascalCase()}}RootWidget extends StatelessWidget {
   const {{name.pascalCase()}}RootWidget({
-    required Key key,
+     Key? key,
   }) : super(key: key);
 
   @override
